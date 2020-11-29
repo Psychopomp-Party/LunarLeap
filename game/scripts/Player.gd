34cedd1e2a00f7a5bf720749fa1982e63ba38387
psychopomp <psychopomp@digitalworld.one>
2020-11-29 14:41:09 -0700

    Improved character movement

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,13 +1,14 @@
 extends KinematicBody
 
+onready var camera = self.get_node("Camera")
 onready var moon = self.get_parent().get_node("Moon")
-onready var moon_direction = self.translation.direction_to(moon.translation).normalized()
+onready var moon_direction = self.translation.direction_to(moon.translation)
 
 # movement variables
 var should_move_left = false
 var should_move_right = false
-var should_move_up = false
-var should_move_down = false
+var should_move_forward = false
+var should_move_backward = false
 
 func _ready():
 	pass
@@ -25,35 +26,39 @@ func _physics_process(delta):
 	#if (self.rotation != old_rotation):
 	#	print("Pointed rotation: ", (moon.translation - self.translation).normalized())
 	
-	moon_direction = self.translation.direction_to(moon.translation).normalized()
-	#self.look_at(moon.translation, moon_direction)
+	moon_direction = self.translation.direction_to(moon.translation)
+	#self.transform.basis.y = -moon_direction
+	
+	#if (moon_direction.cross(self.transform.basis.y).normalized() != Vector3(0, 0, 0)):
+	#	print(moon_direction.cross(self.transform.basis.y).normalized())
+	#	self.rotate(moon_direction.cross(self.transform.basis.y).normalized(), self.transform.basis.y.angle_to(moon.translation))
+	
 	#self.rotate(self.rotation.cross(moon_direction).normalized(), self.rotation.dot(moon_direction))
 	#self.transform = self.transform.orthonormalized()
 	
 	# 'gravity'
 	self.move_and_collide((moon.translation - self.translation).normalized(), true, true, false)
+	#self.rotate(self.transform.basis.x, moon_direction.x)
 	
 	# keep updating movement while button is pressed
 	if (should_move_left):
 		print(">  ", self.rotation.dot(moon_direction))
-		print(">> ", self.rotation.cross(moon_direction).normalized())
-		self.move_and_slide(Vector3(10, 0, 0), Vector3(0, 1, 0), false, 8, 0.667, true)
-		self.look_at(moon.translation, moon_direction)
+		print(">> ", self.transform.basis.y.cross(moon_direction).normalized())
+		#self.move_and_slide(Vector3(-10, 0, 0), -moon_direction, false, 8, 0.667, true)
+		self.rotate(self.transform.basis.y, 0.015)
 	if (should_move_right):
 		print(">  ", self.rotation.dot(moon_direction))
 		print(">> ", self.rotation.cross(moon_direction).normalized())
-		self.move_and_slide(Vector3(-10, 0, 0), Vector3(0, 1, 0), false, 8, 0.667, true)
-		self.look_at(moon.translation, moon_direction)
-	if (should_move_up):
+		#self.move_and_slide(Vector3(10, 0, 0), -moon_direction, false, 8, 0.667, true)
+		self.rotate(self.transform.basis.y, -0.015)
+	if (should_move_forward):
 		print(">  ", self.rotation.dot(moon_direction))
 		print(">> ", self.rotation.cross(moon_direction).normalized())
-		self.move_and_slide(Vector3(0, 10, 0), Vector3(0, 1, 0), false, 8, 0.667, true)
-		self.look_at(moon.translation, moon_direction)
-	if (should_move_down):
+		self.move_and_slide(-self.transform.basis.z * 10, -moon_direction, false, 8, 0.667, true)
+	if (should_move_backward):
 		print(">  ", self.rotation.dot(moon_direction))
 		print(">> ", self.rotation.cross(moon_direction).normalized())
-		self.move_and_slide(Vector3(0, -10, 0), Vector3(0, 1, 0), false, 8, 0.667, true)
-		self.look_at(moon.translation, -moon_direction)
+		self.move_and_slide(self.transform.basis.z * 10, -moon_direction, false, 8, 0.667, true)
 	
 	# set whether the player should move when a button is pressed
 	if (Input.is_action_just_pressed("player_move_left")):
@@ -65,10 +70,10 @@ func _physics_process(delta):
 	if (Input.is_action_just_released("player_move_right")):
 		should_move_right = false
 	if (Input.is_action_just_pressed("player_move_up")):
-		should_move_up = true
+		should_move_forward = true
 	if (Input.is_action_just_released("player_move_up")):
-		should_move_up = false
+		should_move_forward = false
 	if (Input.is_action_just_pressed("player_move_down")):
-		should_move_down = true
+		should_move_backward = true
 	if (Input.is_action_just_released("player_move_down")):
-		should_move_down = false
+		should_move_backward = false
