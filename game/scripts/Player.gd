71433e4ca5bf88d3fd421afc44297063849777fb
psychopomp <psychopomp@digitalworld.one>
2020-11-29 14:41:17 -0700

    Back to 'KinematicBody' for player; improved movement

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,44 +1,63 @@
-extends RigidBody
+extends KinematicBody
 
 onready var gravity_area = self.get_parent().get_node("GravityModifier")
 onready var camera = self.get_node("Camera")
+onready var ray = self.get_node("RayCast")
 onready var moon = self.get_parent().get_node("Moon")
 
-var move_force = 10.0
-var move_velocity = Vector3()
-var jump_force = 0.5
-var jump_velocity = Vector3()
-var torque = Vector3()
+var move_speed = 25.0
+var velocity = Vector3(0.0, 0.0, 0.0)
+
+var rotation_speed = 2.5
+var player_rotation = 0.0
+
+var jump_speed = 25.0
+var is_jumping = false
 
 func _ready():
 	camera.global_transform.basis.z = -camera.global_transform.origin.direction_to(self.global_transform.origin)
-	self.set_axis_lock(PhysicsServer.BODY_AXIS_ANGULAR_X, true)
-	self.set_axis_lock(PhysicsServer.BODY_AXIS_ANGULAR_Z, true)
 
 func _process(delta):
+	var movement = Vector3(0.0, 0.0, 0.0)
+	var rot = 0.0
 	if (Input.is_action_pressed("player_move_forward")):
-		move_velocity = -self.transform.basis.z * move_force
+		movement += -self.transform.basis.z * move_speed
 	if (Input.is_action_pressed("player_move_backward")):
-		move_velocity = self.transform.basis.z * move_force
-	if (Input.is_action_just_pressed("player_move_jump")):
-		jump_velocity = self.transform.basis.y * jump_force
-	if (Input.is_action_just_released("player_move_jump")):
-		jump_velocity = Vector3()
-
-func _integrate_forces(state):
-	# keep upright orientation
-	state.transform.basis.y = -(moon.transform.origin - state.transform.origin).normalized()
+		movement += self.transform.basis.z * move_speed
+	if (Input.is_action_pressed("player_move_left")):
+		rot += rotation_speed
+	if (Input.is_action_pressed("player_move_right")):
+		rot += -rotation_speed
+	if (Input.is_action_pressed("player_move_jump")):
+		pass
 	
-	# apply gravity
-	state.add_central_force(-state.transform.basis.y * 10)
-	
-	# apply movement
-	state.add_central_force(move_velocity)
-	
-	if (state.get_contact_count() > 0):
-		print(state.get_contact_collider_object(0) == moon)
+	velocity = movement
+	player_rotation = rot
 
 func _physics_process(delta):
-	if (self.linear_velocity != Vector3() && self.linear_velocity < move_velocity):
-		self.add_central_force(move_velocity)
-	self.apply_impulse(Vector3.UP, jump_velocity)
+	
+	# orient the player on the moon
+	var moon_direction = self.transform.origin.direction_to(moon.transform.origin)
+	var player_down = -self.transform.basis.y
+	var axis = player_down.cross(moon_direction)
+	var phi = player_down.angle_to(moon_direction)
+	if (axis.length_squared() == 0):
+		axis = -self.transform.basis.z
+	else:
+		axis = axis.normalized()
+		self.transform.basis = self.transform.basis.rotated(axis, phi)
+		self.transform = self.transform.orthonormalized()
+	
+	# apply gravity if not currently colliding with the moon
+	# NOTE: this will probably only be necessary once jumping is implemented
+	if (ray.is_colliding()):
+		#velocity += -ray.get_collision_normal() * 10.
+		pass
+	else:
+		#velocity += moon_direction * 10.0
+		pass
+	
+	self.move_and_collide(velocity * delta)
+	
+	# rotate
+	self.rotate(self.transform.basis.y, player_rotation * delta)
